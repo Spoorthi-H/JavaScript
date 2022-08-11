8bcfe8d3b94deabf9a58779a814fd3704ef356e0
Your NameSpoorthi-H <spoorthihm51@gmail.com>
2022-08-11 15:52:23 +0530

    JS

---
+++ arr.js
@@ -0,0 +1,10 @@
+var ids=[01,102,103]
+var size=[]
+var sizes=[38,40,42]
+var arr=[10,20,10,'abc',true,{id:101}]
+console.log(ids)
+console.log(size)
+console.log(sizes)
+console.log(arr)
+console.log(typeof sizes)
+console.log(typeof arr)
---
+++ one.js
@@ -0,0 +1,15 @@
+var rating=2.3
+var brand='Samsung'
+var price=5999
+var dis=true
+var stock
+console.log(brand)
+console.log(price)
+console.log(typeof brand)
+console.log(typeof price)
+console.log(rating)
+console.log(typeof rating)
+console.log(dis)
+console.log(typeof dis)
+console.log(stock)
+console.log(typeof stock)
