2f58a07cff889ce3db4746237ab02ca71d7822d8
Your NameSpoorthi-H <spoorthihm51@gmail.com>
2022-08-20 12:55:45 +0530

    Events

---
+++ JSEvents/event.js
@@ -0,0 +1,24 @@
+function changeColorover()
+{
+    document.getElementById("btn1").style.backgroundColor="aqua"
+}
+function changeColorout()
+{
+    document.getElementById("btn2").style.backgroundColor="pink"
+}
+function changeColorclk()
+{
+    document.getElementById("btn3").style.backgroundColor="purple"
+}
+function changeColordblclk()
+{
+    document.getElementById("btn4").style.backgroundColor="brown"
+}
+function changeColorfocus()
+{
+    document.getElementById("ip1").style.backgroundColor="yellow"
+}
+function changeColorblur()
+{
+    document.getElementById("ip2").style.backgroundColor="violet"
+}
